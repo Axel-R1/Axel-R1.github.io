668375e9869876c5551cc855be4923091f0ab503
AXEL <44101382+Axel-R1@users.noreply.github.com>
2018-11-02 23:18:53 +0400

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,18 @@
+﻿$(function(){
+
+	var hf = function(){
+		var h_header = $('header').height();
+		var h_footer = $('footer').height();
+		$('.content').css({
+			'paddingTop': h_header,
+			'paddingBottom': h_footer
+		});
+	}
+
+	$(window).bind('load resize', hf);
+
+});
+
+   
+
+
